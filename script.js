d54c9fd7b8541579daf9745e3d51f8e771c1a087
chaeeun <bce0102@gmail.com>
2024-04-30 00:38:17 +0900

    영화 검색 구현

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", opti
 
     movies.forEach((movie) => {
       const movieCard = document.createElement("div");
+      movieCard.className = "card-list";
       movieCard.innerHTML = `
-            <img class="movie-poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-            <h3 class="movie-title">${movie.title}</h3>
+            <img class="movie-poster" id="${movie.id}" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+            <h3 class="movieTitle">${movie.title}</h3>
             <p>${movie.overview}</p>
             <p>Rating: ${movie.vote_average}</p>
-            <p class="movie-id">영화 id: ${movie.id}</p>
         `;
       movieID.appendChild(movieCard);
     });
@@ -33,11 +33,37 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", opti
     // 각 포스터 이미지에 클릭 이벤트 리스너 추가
     const posters = document.querySelectorAll(".movie-poster");
     posters.forEach((poster, index) => {
-      poster.addEventListener("click", () => {
+      poster.addEventListener("click", (movie) => {
         const movieId = movies[index].id;
-        alert(`영화 ID: ${movieId}`);
+        console.log(poster.id);
+        alert(`영화 ID: ${poster.id}`);
       });
     });
+
+    // 검색 창
+    const addSearchBar = document.createElement("input");
+    addSearchBar.setAttribute("id", "search-input");
+
+    // 검색 버튼 클릭 시 해당 문자열 포함된 영화 웹페이지 출력
+    const addSearchBtn = document.querySelector("#search");
+    addSearchBtn.setAttribute("onclick", "searchMovieData()");
+
+    // 영화 이름 검색 시 해당 문자열 포함된 영화 웹페이지 출력
+    searchMovieData = () => {
+      const value = document.getElementById("search-input").value.toUpperCase();
+      const cardData = document.getElementsByClassName("card-list");
+      // const movieTitle = document.getElementsByClassName("movie-title");
+      let movieTitle;
+      for (let i = 0; i < cardData.length; i++) {
+        movieTitle = cardData[i].getElementsByClassName("movieTitle");
+        // God father
+        if (movieTitle[0].innerHTML.toUpperCase().indexOf(value) > -1) {
+          cardData[i].style.display = "block";
+        } else {
+          cardData[i].style.display = "none";
+        }
+      }
+    };
   })
   .catch((err) => {
     console.error("에러 발생", err); // 에러 처리
